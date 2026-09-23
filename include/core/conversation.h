e9e9ea2fd86927b89a23b3499f8716112c6d2a71
Hector Perez <hectorp0617@gmail.com>
2026-09-22 21:36:06 -0400

    Conversation destructor

--- include/core/conversation.h
+++ include/core/conversation.h
@@ -9,6 +9,42 @@
 class Conversation {
 public:
     Conversation() = default;
+    
+    // Destructor to clean up allocated memory
+    ~Conversation() {
+        delete[] data_;
+    }
+
+    void append(Message m) {
+        if (size_ == capacity_) {
+            // Handle capacity overflow (e.g., reallocate with increased capacity)
+            std::size_t new_capacity;
+
+            if (capacity_ == 0) {
+                new_capacity = 1; // Start with a capacity of 1
+            } else {
+                new_capacity = capacity_ * 2; // Double the capacity
+            }
+
+            Message* new_data = new Message[new_capacity];
+
+            try {
+                for (std::size_t i = 0; i < size_; ++i) {
+                    new_data[i] = data_[i]; // Copy existing messages to new array
+                }
+            } catch (...) {
+                delete[] new_data; // Clean up if an exception occurs
+                throw; // Rethrow the exception
+            }
+
+            delete[] data_; // Free the old array
+            data_ = new_data; // Update data_ to point to the new array
+            capacity_ = new_capacity; // Update capacity_
+        }
+
+        data_[size_] = m; // Add the new message to the end of the conversation
+        ++size_; // Increment the size of the conversation
+    }
 
     std::size_t size() const noexcept {
         // Return the number of messages in the conversation
